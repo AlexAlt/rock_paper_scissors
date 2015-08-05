83e06bb49c7df240afa88b9ecc3c0d3110b0683a
Alex M Altieri & Tony Navarro <student@epicodus.com>
2015-08-05 15:15:01 -0700

    writes first integration spec for RPS project

---
+++ spec/rps_integration_specs.rb
@@ -0,0 +1,14 @@
+require('capybara/rspec')
+require('./app')
+Capybara.app = Sinatra::Applications
+set(:show_expectations, false)
+
+describe('the rock_paper_scissors path', {:type => :feature}) do
+  it('processes the two inputs and returns true')
+  visit('/')
+
+  fill_in("player_1" :with "rock")
+
+  click_button('Enter')
+  expect(page).to have_content(true)
+end
