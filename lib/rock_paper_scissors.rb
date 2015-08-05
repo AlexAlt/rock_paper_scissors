9ccaada4ba9cbbc06afd33844455734477dce3a7
Alex M Altieri & Tony Navarro <student@epicodus.com>
2015-08-05 14:19:42 -0700

    Code passes all other specs for winning combos, passes specs for ties.

--- lib/rock_paper_scissors.rb
+++ lib/rock_paper_scissors.rb
@@ -2,6 +2,12 @@ class String
   define_method(:beats?) do |player_2|
     if  self == "rock" && player_2 == "scissors"
       return true
+    elsif self == "scissors" && player_2 == "paper"
+      return true
+    elsif self == "paper" && player_2 == "rock"
+      return true
+    elsif self == player_2
+      return "Tie!"
     end
 
     # else
